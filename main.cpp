eac0d7706221ddfb8b02148c2bcfcc6f0109102b
Matteo Schmider <matteoschmider@Matteos-iMac-2.fritz.box>
2019-07-07 00:23:20 +0200

    Yellow channel calculation different

--- main.cpp
+++ main.cpp
@@ -1,33 +1,76 @@
-/*#include <stdio.h>
-
-int main (int argc, char **argv) 
-{
-	printf("Hello, world!\n");
-	return 0;
-}*/
-
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/core.hpp"
+
 using namespace cv;
 using namespace std;
-int main(int argc, char **argv) {
+
+Mat gray;
+Mat blue;
+Mat green;
+Mat red;
+Mat yellow;
+Mat black;
+Mat ryG;
+Mat gyG;
+
+Mat cameraFrame;
+
+Mat channels[3];
+
+int main(int argc, const char * argv[]) {
     //Capture stream from webcam.
     VideoCapture capture("rkcamsrc io-mode=4 isp-mode=2A ! video/x-raw,format=NV12,width=640,height=480 ! videoconvert ! appsink");
 
     //Check if we can get the webcam stream.
-    if(!capture.isOpened())
-    {
-        printf("Could not open camera\n");
+    if(!capture.isOpened()) {
+        cout << "Could not open camera" << endl;
         return -1;
     }
-    printf("Opened Camera\n");
-    while (true)
-    {
-		Mat cameraFrame;
-		capture.read(cameraFrame);
-		imshow("Cam View", cameraFrame);
-    	if (waitKey(50) >= 0)
-			break;
+    while (true) {
+        //Read an image from the camera.
+        capture.read(cameraFrame);
+		
+		//cameraFrame = imread("/Users/matteoschmider/Desktop/Foto.png", IMREAD_COLOR);
+		
+		split(cameraFrame, channels);
+		blue = channels[0];
+		green = channels[1];
+		red = channels[2];
+		
+		cvtColor(cameraFrame, gray, COLOR_BGR2GRAY);
+		subtract(blue, gray, blue);
+		subtract(green, gray, green);
+		subtract(red, gray, red);
+		
+		multiply(red, red, red);
+		threshold(red, red, 127, 255, THRESH_BINARY);
+		
+		/*extractChannel(cameraFrame, red, 2);
+		extractChannel(cameraFrame, green, 1);
+		extractChannel(cameraFrame, blue, 0);*/
+		
+		//addWeighted(blue, -1.0, blue, 0.0, 255.0, yellow);
+		absdiff(green, red, yellow);
+		subtract(yellow, channels[0], yellow);
+		// above equals: yellow = 1 - blue
+		
+		absdiff(Scalar(255), gray, black);
+		
+    	imshow("Original Image", cameraFrame);
+		imshow("Ball", red);
+		imshow("Blue Goal", blue);
+		imshow("Yellow Goal", yellow);
+		//imshow("Field", green);
+		//imshow("Walls", black);
+		
+		char key = (char) waitKey(30);
+        if (key == 'q' || key == 27)
+        {
+            break;
+        }
 	}
- 	return 0;
+	return 0;
 }
