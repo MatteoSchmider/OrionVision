2c99ddff4af0a7f59df638e05b5edf939fdc94a3
Matteo Schmider <matteoschmider@Matteos-iMac-2.fritz.box>
2019-07-07 00:28:15 +0200

    First auto-update implementation

--- update_start.sh
+++ update_start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 cd ..
-ls
+rm -rf OrionVision
+git clone https://github.com/MatteoSchmider/OrionVision.git
+cd OrionVision
+g++ main.cpp -o main `pkg-config --cflags --libs opencv`
+./main
