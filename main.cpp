0e42dc1600690f91c87ae3f226962b6b36aa20b5
Matteo Schmider <matteoschmider@Matteos-iMac-2.fritz.box>
2019-07-08 22:38:28 +0200

    Hey

--- main.cpp
+++ main.cpp
@@ -70,7 +70,7 @@ int main(int argc, const char * argv[]) {
 		
     	imshow("Original Image", cameraFrame);
 		imshow("Gray", gray);
-		//imshow("Ball", red);
+		imshow("Ball", red);
 		//imshow("Blue Goal", blue);
 		//imshow("Yellow Goal", yellow);
 		//imshow("Field", green);
