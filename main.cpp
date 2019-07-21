f66f451d9ef651c1fa75ff8d756b27ad0194be35
Matteo Schmider <matteoschmider@Matteos-iMac-2.fritz.box>
2019-07-21 21:57:30 +0200

    gamma correction - working on image quality

--- main.cpp
+++ main.cpp
@@ -24,15 +24,15 @@ UMat channels[3];
 
 cv::TickMeter tm;
 
-Mat correctGamma( Mat& img, double gamma ) {
+UMat correctGamma( UMat& img, double gamma ) {
  double inverse_gamma = 1.0 / gamma;
  
- Mat lut_matrix(1, 256, CV_8UC1 );
+ UMat lut_matrix(1, 256, CV_8UC1 );
  uchar * ptr = lut_matrix.ptr();
  for( int i = 0; i < 256; i++ )
    ptr[i] = (int)( pow( (double) i / 255.0, inverse_gamma ) * 255.0 );
  
- Mat result;
+ UMat result;
  LUT( img, lut_matrix, result );
  
  return result;
