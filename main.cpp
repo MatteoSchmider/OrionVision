9b6c89d49bc437df41104d9d0dd98267764eec76
MatteoSchmider <matteoschmider@aol.com>
2019-07-30 16:07:42 +0200

    vsd

--- main.cpp
+++ main.cpp
@@ -89,27 +89,13 @@ void normalizeChannels() {
 void doContours() {
         vector<vector<Point> > contours;
         vector<Vec4i> hierarchy;
-        findContours( redThreshold, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0) );
+        findContours(redThreshold, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0) );
         vector<vector<Point> > contours_poly( contours.size() );
-        vector<Rect> boundRect( contours.size() );
-        vector<Point2f>center( contours.size() );
-        vector<float>radius( contours.size() );
 
-        for( int i = 0; i < contours.size(); i++ )
-        { approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
-          boundRect[i] = boundingRect( Mat(contours_poly[i]) );
-          minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );}
-
-
-        /// Draw polygonal contour + bonding rects + circles
-        Mat drawing;
-        cameraFrame.copyTo(drawing);
         for( int i = 0; i< contours.size(); i++ )
         {
                 Scalar color = Scalar(255, 0, 0);
-                //drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
-                rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
-                circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
+                drawContours( cameraFrame, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
         }
 
         /// Show in a window
