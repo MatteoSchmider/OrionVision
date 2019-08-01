b6317a35e599e3067851e9744f2b3817b1ba021f
MatteoSchmider <matteoschmider@aol.com>
2019-08-01 11:21:54 +0200

    bugfix

--- main.cpp
+++ main.cpp
@@ -219,7 +219,7 @@ int main(int argc, const char * argv[]) {
         int count;
         unsigned int nextTime;
         while (true) {
-                if ((fd = serialOpen ("/dev/ttyAMA0", 115200)) < 0)
+                if ((fd = serialOpen ("/dev/ttyS1", 115200)) < 0)
                 {
                         fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno));
                         return 1;
