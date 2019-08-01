0993c612076cf1a2bded18c62553e158e4e75cf5
MatteoSchmider <matteoschmider@aol.com>
2019-08-01 11:31:14 +0200

    No memory error

--- main.cpp
+++ main.cpp
@@ -253,7 +253,7 @@ int main(int argc, const char * argv[]) {
                 }
                 // }
 
-                printf ("\n");
+                // printf ("\n");
         }
         return 0;
 }
