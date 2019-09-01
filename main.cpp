d8bf3a642adbcfcfdd40c6c00ae01b0e4c3534aa
MatteoSchmider <matteoschmider@aol.com>
2019-09-01 17:29:42 +0200

    added white balance code from internet

--- main.cpp
+++ main.cpp
@@ -60,6 +60,53 @@ int fd;
 char teensyByte = 0;
 bool robotOnField = false;
 
+void balance_white(Mat mat) {
+        double discard_ratio = 0.05;
+        int hists[3][256];
+        memset(hists, 0, 3*256*sizeof(int));
+
+        for (int y = 0; y < mat.rows; ++y) {
+                uchar* ptr = mat.ptr<uchar>(y);
+                for (int x = 0; x < mat.cols; ++x) {
+                        for (int j = 0; j < 3; ++j) {
+                                hists[j][ptr[x * 3 + j]] += 1;
+                        }
+                }
+        }
+
+        // cumulative hist
+        int total = mat.cols*mat.rows;
+        int vmin[3], vmax[3];
+        for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 255; ++j) {
+                        hists[i][j + 1] += hists[i][j];
+                }
+                vmin[i] = 0;
+                vmax[i] = 255;
+                while (hists[i][vmin[i]] < discard_ratio * total)
+                        vmin[i] += 1;
+                while (hists[i][vmax[i]] > (1 - discard_ratio) * total)
+                        vmax[i] -= 1;
+                if (vmax[i] < 255 - 1)
+                        vmax[i] += 1;
+        }
+
+
+        for (int y = 0; y < mat.rows; ++y) {
+                uchar* ptr = mat.ptr<uchar>(y);
+                for (int x = 0; x < mat.cols; ++x) {
+                        for (int j = 0; j < 3; ++j) {
+                                int val = ptr[x * 3 + j];
+                                if (val < vmin[j])
+                                        val = vmin[j];
+                                if (val > vmax[j])
+                                        val = vmax[j];
+                                ptr[x * 3 + j] = static_cast<uchar>((val - vmin[j]) * 255.0 / (vmax[j] - vmin[j]));
+                        }
+                }
+        }
+}
+
 Mat correctGamma(Mat& img) {
         double inverse_gamma = 100 / gammaSlider;
         Mat lut_matrix(1, 256, CV_8UC1 );
@@ -73,6 +120,7 @@ Mat correctGamma(Mat& img) {
 
 void prepareFrame() {
         cameraFrameNoMask.copyTo(cameraFrameNoGamma, mask);
+        balance_white(cameraFrameNoGamma);
         //Gamma
         cameraFrameNoBlur = correctGamma(cameraFrameNoGamma);
         //blur
