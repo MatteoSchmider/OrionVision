8764988ef58dd400ffc0102ee340ac5b5ad97a5f
MatteoSchmider <matteoschmider@aol.com>
2019-09-02 15:05:53 +0200

    immer no net

--- main.cpp
+++ main.cpp
@@ -62,26 +62,20 @@ char teensyByte = 0;
 bool robotOnField = false;
 
 void SimplestCB(Mat& in, Mat& out, float percent) {
-    Mat ycrcb;
-
-    cvtColor(in, ycrcb, COLOR_BGR2YCrCb);
-
-    vector<Mat> channels;
-    split(ycrcb,channels);
-
-    equalizeHist(channels[0], channels[0]);
-
-    merge(channels,ycrcb);
-
-    cvtColor(ycrcb,out, COLOR_YCrCb2BGR);
+        Mat lookUpTable(1, 256, CV_8U);
+        uchar* p = lookUpTable.ptr();
+        for( int i = 0; i < 256; ++i)
+            p[i] = saturate_cast<uchar>(pow(i / 255.0, (percent/100)) * 255.0);
+        //out = in.clone();
+        LUT(in, lookUpTable, out);
 }
 
 void prepareFrame() {
         /*Ptr<xphoto::GrayworldWB> var = xphoto::createGrayworldWB();
            var->setSaturationThreshold(gammaSlider);
            var->balanceWhite(cameraFrameNoMask, cameraFrameNoMask);*/
-        SimplestCB(cameraFrameNoMask, cameraFrameNoMask, (float) gammaSlider);
-        //cameraFrameNoMask.convertTo(cameraFrameNoMask, -1, 1, 100);
+        //SimplestCB(cameraFrameNoMask, cameraFrameNoMask, (float) gammaSlider);
+        cameraFrameNoMask.convertTo(cameraFrameNoMask, -1, 1, 50);
         cameraFrameNoMask.copyTo(cameraFrameNoBlur);//, mask);
         //blur
         blur(cameraFrameNoBlur, cameraFrame, Size(1, 1));
