c0e22680ba20ced3564afbaaf18d2f280c8b8633
MatteoSchmider <matteoschmider@aol.com>
2019-09-02 11:30:21 +0200

    equalizeHist on all the channels

--- main.cpp
+++ main.cpp
@@ -65,23 +65,7 @@ void SimplestCB(Mat& in, Mat& out, float percent) {
         float half_percent = percent / 200.0f;
         vector<Mat> tmpsplit; split(in,tmpsplit);
         for(int i=0; i<3; i++) {
-                /*//find the low and high precentile values (based on the input percentile)
-                   Mat flat; tmpsplit[i].reshape(1,1).copyTo(flat);
-                   cv::sort(flat,flat,SORT_EVERY_ROW + SORT_ASCENDING);
-                   int lowval = flat.at<uchar>(cvFloor(((float)flat.cols) * half_percent));
-                   int highval = flat.at<uchar>(cvCeil(((float)flat.cols) * (1.0 - half_percent)));*/
-                /*double low;
-                   double high;
-                   minMaxIdx(tmpsplit[i], &low, &high);
-                   int lowval = (int) (low * half_percent);
-                   int highval = (int) (high * (1.0 - half_percent));
-
-                   //saturate below the low percentile and above the high percentile
-                   tmpsplit[i].setTo(lowval,tmpsplit[i] < lowval);
-                   tmpsplit[i].setTo(highval,tmpsplit[i] > highval);*/
-
-                //scale the channel
-                normalize(tmpsplit[i],tmpsplit[i],0,255,NORM_MINMAX);
+                equalizeHist(tmpsplit[i], tmpsplit[i]);
         }
         merge(tmpsplit,out);
 }
