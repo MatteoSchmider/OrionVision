27f37acb36a2c4673a540c1eba4ebe7acf399092
MatteoSchmider <matteoschmider@aol.com>
2019-09-02 11:03:09 +0200

    better performance maybe?

--- main.cpp
+++ main.cpp
@@ -65,12 +65,14 @@ void SimplestCB(Mat& in, Mat& out, float percent) {
         float half_percent = percent / 200.0f;
         vector<Mat> tmpsplit; split(in,tmpsplit);
         for(int i=0; i<3; i++) {
-                //find the low and high precentile values (based on the input percentile)
-                Mat flat; tmpsplit[i].reshape(1,1).copyTo(flat);
-                cv::sort(flat,flat,SORT_EVERY_ROW + SORT_ASCENDING);
-                int lowval = flat.at<uchar>(cvFloor(((float)flat.cols) * half_percent));
-                int highval = flat.at<uchar>(cvCeil(((float)flat.cols) * (1.0 - half_percent)));
-                cout << lowval << " " << highval << endl;
+                /*//find the low and high precentile values (based on the input percentile)
+                   Mat flat; tmpsplit[i].reshape(1,1).copyTo(flat);
+                   cv::sort(flat,flat,SORT_EVERY_ROW + SORT_ASCENDING);
+                   int lowval = flat.at<uchar>(cvFloor(((float)flat.cols) * half_percent));
+                   int highval = flat.at<uchar>(cvCeil(((float)flat.cols) * (1.0 - half_percent)));*/
+                minMaxIdx(tmpsplit[i], low, high);
+                int lowval = (int) (low * half_percent);
+                int highval = (int) (high * (1.0 - half_percent));
 
                 //saturate below the low percentile and above the high percentile
                 tmpsplit[i].setTo(lowval,tmpsplit[i] < lowval);
