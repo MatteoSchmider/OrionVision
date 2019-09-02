6c643c0dda4d054117ca4c79de53e149d0616d81
MatteoSchmider <matteoschmider@aol.com>
2019-09-02 12:29:45 +0200

    ball angle subject to divide by zero

--- main.cpp
+++ main.cpp
@@ -158,7 +158,7 @@ void doContours() {
                 cout << "Ball X: " << ballX << endl;
                 cout << "Ball Y: " << ballY << endl;
                 int ballradius = (int) sqrt((ballX * ballX) + (ballY * ballY));
-                double angle = (double) tan(ballY / ballX) * 180;
+                //double angle = (double) tan(ballY / ballX) * 180;
                 cout << "Ball Radius: " << ballradius << endl;
                 cout << "Ball Angle: " << angle << endl;
 
