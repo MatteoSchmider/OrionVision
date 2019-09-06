cc8730535fee26dc9e7ff4bc075291a3ab837be9
MatteoSchmider <matteoschmider@aol.com>
2019-09-06 10:56:05 +0200

    no more games

--- main.cpp
+++ main.cpp
@@ -111,7 +111,7 @@ void normalizeChannels() {
         //yellow goal
         absdiff(red, green, yellow);
         subtract(red, yellow, yellow);
-        subtract(yellow * 2.0, blue * 3.0, yellow);
+        subtract(yellow, blue, yellow);
         subtract(yellow, redNormalized, yellowNormalized);
 }
 
