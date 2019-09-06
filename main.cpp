cdae287cfe9e9e864326dd83a0c54f5122e7d78e
MatteoSchmider <matteoschmider@aol.com>
2019-09-06 10:19:17 +0200

    yellow dilate erode, blue is perfect

--- main.cpp
+++ main.cpp
@@ -240,6 +240,7 @@ void processFrame() {
         prepareFrame();
         normalizeChannels();
         erode(blueNormalized, blueEroded, Mat());
+        dilate(yellowNormalized, yellowNormalized, Mat());
         erode(yellowNormalized, yellowEroded, Mat());
         double redTresh = maxr * (double)(threshold_red_slider / 100.0);
         double blueTresh = maxb * (double)(threshold_blue_slider / 100.0);
