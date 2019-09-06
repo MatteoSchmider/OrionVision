f9651c88825f6ae404faf67026264daabda8dde6
MatteoSchmider <matteoschmider@aol.com>
2019-09-06 10:36:44 +0200

    mat constructor call in function call error

--- main.cpp
+++ main.cpp
@@ -241,7 +241,7 @@ void processFrame() {
         normalizeChannels();
         erode(blueNormalized, blueEroded, Mat());
         //dilate(yellowNormalized, yellowNormalized, Mat());
-        erode(yellowNormalized, yellowEroded, Mat(5, 5));
+        erode(yellowNormalized, yellowEroded, new Mat(5, 5));
         double redTresh = maxr * (double)(threshold_red_slider / 100.0);
         double blueTresh = maxb * (double)(threshold_blue_slider / 100.0);
         double yellowTresh = maxy * (double)(threshold_yellow_slider / 100.0);
