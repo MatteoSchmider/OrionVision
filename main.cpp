cdd6dab4a54717390cf896b47d0e6c2200bab163
MatteoSchmider <matteoschmider@aol.com>
2019-09-08 19:11:22 +0200

    output x and y for debug

--- main.cpp
+++ main.cpp
@@ -256,8 +256,8 @@ void doContours() {
                 ballY = (int) (minRectBall[0].center.y);
                 ballY -= CENTER_Y;
                 ballVisible = true;
-                //cout << "Ball X: " << ballX << endl;
-                //cout << "Ball Y: " << ballY << endl;
+                cout << "Ball X: " << ballX << endl;
+                cout << "Ball Y: " << ballY << endl;
                 double ballradiusDouble = sqrt((ballX * ballX) + (ballY * ballY));
                 ballRadius = (18.38108 - (0.000427424254 * (1 - exp(0.05804322 * ballradiusDouble))));
                 //ballX = ballRadius * cos(ballAngle);
