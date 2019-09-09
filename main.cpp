04e6a2e3b51060a0e8b8419dd886ce5d1ce5e8ef
MatteoSchmider <matteoschmider@aol.com>
2019-09-09 09:57:58 +0200

    output 4 again

--- main.cpp
+++ main.cpp
@@ -262,14 +262,22 @@ void doContours() {
                                 line(cameraFrame, Point(CENTER_X, CENTER_Y), Point(CENTER_X + (ballX * 2.0), CENTER_Y + (ballY * 2.0)), Scalar(0, 255, 0));
                         }
                         else {
+                                ballAngle = tangents1();
+                                ballX = ballRadius * cos(ballAngle / (180 / PI));
+                                ballY = ballRadius * sin(ballAngle / (180 / PI));
+                                line(cameraFrame, Point(CENTER_X, CENTER_Y), Point(CENTER_X + (ballX * 2.0), CENTER_Y + (ballY * 2.0)), Scalar(255, 0, 0));
                                 ballAngle = tangents2();
                                 ballX = ballRadius * cos(ballAngle / (180 / PI));
                                 ballY = ballRadius * sin(ballAngle / (180 / PI));
                                 line(cameraFrame, Point(CENTER_X, CENTER_Y), Point(CENTER_X + (ballX * 2.0), CENTER_Y + (ballY * 2.0)), Scalar(0, 255, 0));
-                                ballAngle = tangents4();
+                                ballAngle = tangents3();
                                 ballX = ballRadius * cos(ballAngle / (180 / PI));
                                 ballY = ballRadius * sin(ballAngle / (180 / PI));
                                 line(cameraFrame, Point(CENTER_X, CENTER_Y), Point(CENTER_X + (ballX * 2.0), CENTER_Y + (ballY * 2.0)), Scalar(0, 0, 255));
+                                ballAngle = tangents4();
+                                ballX = ballRadius * cos(ballAngle / (180 / PI));
+                                ballY = ballRadius * sin(ballAngle / (180 / PI));
+                                line(cameraFrame, Point(CENTER_X, CENTER_Y), Point(CENTER_X + (ballX * 2.0), CENTER_Y + (ballY * 2.0)), Scalar(255, 0, 255));
                         }
                 }
                 else {
