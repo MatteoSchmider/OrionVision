aebe4c2bc9bb163e4a249e390b23aca71c565759
MatteoSchmider <matteoschmider@aol.com>
2019-09-09 10:05:46 +0200

    double minus

--- main.cpp
+++ main.cpp
@@ -275,7 +275,7 @@ void doContours() {
                                    ballY = ballRadius * sin(ballAngle / (180 / PI));
                                    line(cameraFrame, Point(CENTER_X, CENTER_Y), Point(CENTER_X + (ballX * 2.0), CENTER_Y + (ballY * 2.0)), Scalar(0, 0, 255));
                                  */
-                                ballAngle = tangents4() + tangents2();
+                                ballAngle = tangents4() - (tangents2() - tangents4());
                                 ballX = ballRadius * cos(ballAngle / (180 / PI));
                                 ballY = ballRadius * sin(ballAngle / (180 / PI));
                                 line(cameraFrame, Point(CENTER_X, CENTER_Y), Point(CENTER_X + (ballX * 2.0), CENTER_Y + (ballY * 2.0)), Scalar(255, 0, 255));
